b7943a27196781b96815f4287304341a6af2143e
sarahbubu <sbuenoo1204@gmail.com>
2026-02-18 23:27:14 -0300

    adicionando formas de visualizar e separar os dados

--- app.py
+++ app.py
@@ -1,4 +1,25 @@
 import streamlit as st
+import pandas as pd
 
-st.title("Dashboard de Risco Cardíaco")
-st.write("Projeto em construção 😌")
+st.title('Dashboard de Risco Cardíaco🫀')
+
+df = pd.read_csv('data/heart.csv')
+
+st.subheader('Visualização dos dados')
+st.dataframe(df)
+
+st.subheader('Primeira 5 linhas')
+st.write(df.head())
+
+st.subheader('Informações do Dataset')
+st.write('Numero de linhas:' , df.shape[0])
+st.write('Numero de colunas:' , df.shape[1])
+st.write('Colunas:' , df.columns)
+
+st.subheader('Distribuição da Doença Cardíaca')
+st.write(df['num'].value_counts())
+
+df['Doença'] = df['num'].apply(lambda x: 0 if x == 0 else 1)
+
+st.subheader('Distribuição Simplificada')
+st.write(df['Doença'].value_counts())
